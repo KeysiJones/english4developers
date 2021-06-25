aa49e82616123ff2f288e22da36ddbbde3e1a72d
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-06-24 21:07:09 -0300

    Ajuste na opacidade e na cor dos elementos

--- src/App.js
+++ src/App.js
@@ -3,55 +3,64 @@ import './index.css'
 import developer from './images/1280.jpg'
 
 function App() {
+  const sendWppMessage = () => {
+    window.open("https://wa.me/5551989061012?text=Olá Felipe ! quero saber mais sobre o seu curso de Inglês para devs.", '_blank')
+  }
   return (
-    <div className="min-h-screen bg-gray-100 text-left">
-      <div className="bg-black dark:bg-gray-800 p-3">
-        <span className="text-white text-2xl ml-28">English 4 Developers</span>
+    <div className="min-h-screen text-left">
+      <div className="bg-green-400 dark:bg-gray-800 p-3">
+        <span className="text-white text-2xl ml-24 relative font-bold">English 4 Developers</span>
       </div>
-      <div className='h-screen bg-bottom text-left' style={{ backgroundImage: `url(${developer})` }}>
+      <div id='hero'className='h-screen bg-bottom text-left' style={{ backgroundImage: `url(${developer})` }}>
         <div>
-          <p className='text-black text-5xl text-left w-96 p-12 ml-16 leading-tight'>Domine o Inglês e trabalhe de onde quiser.</p>
+          <p className='text-green-500 text-5xl text-left w-96 p-12 ml-16 leading-tight relative'>Domine o Inglês e trabalhe de onde quiser.</p>
         </div>
-        <button className='text-2xl shadow-2xl bg-white rounded-lg p-3 mt-10 ml-28'>Quero saber mais</button>
+        <button onClick={sendWppMessage} className='text-2xl font-bold absolute shadow-2xl bg-green-400 text-white rounded-lg p-3 mt-10 ml-28'>Quero saber mais !</button>
       </div>
-      <h2 className='text-center'>Testemunhos</h2>
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <h2 className='text-center text-white text-4xl my-4 bg-green-400 p-3 relative'>Fica tranquilo, outras pessoas já fizeram o curso.</h2>
+      <div id="carouselExampleIndicators" className="carousel slide mb-10" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner rounded-xl">
-          <div class="text-center active carousel-item m-auto place-content-center">
-            <div className=' m-2 p-2 bg-black text-white w-7/12 rounded text-center h-96'>
-              <h3>Ola</h3>
+        <div className="carousel-inner rounded-xl">
+          <div className="text-center active carousel-item m-auto place-content-center">
+            <div className='m-2 p-2 bg-green-400 block text-white w-7/12 rounded-3xl text-center h-96'>
+              <p className='mx-20 text-3xl mt-20'>"Depois que eu comecei o curso do Deoly eu nunca mais tive medo de fazer uma entrevista em Inglês"</p>
+              <p className='text-2xl mt-14'>- Camila Santos</p>
+              <p className='text-lg'>Desenvolvedora backend</p>
             </div>
           </div>
-          {/*<div class="text-center carousel-item m-auto place-content-center">
-            <div className='m-2 p-2 bg-black text-white w-7/12 rounded text-center h-96'>
-              Mundo
+          {/* <div class="text-center carousel-item m-auto place-content-center">
+            <div className='m-2 p-2 bg-gray-300 grid text-white w-7/12 rounded-3xl text-center h-96'>
+              <p className='mx-20 text-3xl mt-28'>"Depois que eu comecei o curso do Deoly eu nunca mais tive medo de fazer uma entrevista em Inglês"</p>
+              <p className='mx-20 text-2xl'>- Camila Santos</p>
+              <p className='mx-20 text-lg'>Desenvolvedora backend</p>
             </div>
           </div>
           <div class="text-center carousel-item m-auto place-content-center">
-            <div className='m-2 p-2 bg-black text-white w-7/12 rounded text-center h-96'>
-              Hahaha
+            <div className='m-2 p-2 bg-gray-300 grid text-white w-7/12 rounded-3xl text-center h-96'>
+              <p className='mx-20 text-3xl mt-28'>"Depois que eu comecei o curso do Deoly eu nunca mais tive medo de fazer uma entrevista em Inglês"</p>
+              <p className='mx-20 text-2xl'>- Camila Santos</p>
+              <p className='mx-20 text-lg'>Desenvolvedora backend</p>
             </div>
-  </div>*/}
+          </div> */}
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span className="carousel-control-prev-icon bg-green-400 p-6 rounded-lg" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon bg-green-400 p-6 rounded-lg" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
-      <div className='m-auto text-center flex place-content-center'>
+      {/* <div className='m-auto text-center flex place-content-center'>
         <div className='m-2 p-2 bg-black text-white w-60 rounded'>epa</div>
         <div className='m-2 p-2 bg-black text-white w-60 rounded'>epa</div>
         <div className='m-2 p-2 bg-black text-white w-60 rounded'>epa</div>
-      </div>
+      </div> */}
     </div>
   );
 }
