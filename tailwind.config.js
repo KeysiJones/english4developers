085d41a0b3121f5b02ac9f30bc387ba28512ab54
keysijones <keysijones01@gmail.com>
2021-07-27 20:27:45 -0300

    Adicionei novo tamanho para telas muito pequenas.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
+      'xsm': { 'min': '0px', 'max': '600px' },
       'sm': { 'min': '0px', 'max': '767px' },
       'md': { 'min': '768px', 'max': '1023px' },
       'lg': { 'min': '1024px', 'max': '1279px' },
